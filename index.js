44926a70419d2bee288aa06ed9d2b3b86f20d5d5
aryan1403 <aaryan14032006@gmail.com>
2024-07-19 12:33:10 +0530

    apollo server added

--- index.js
+++ index.js
@@ -2,11 +2,41 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4')
 const app = express();
 const PORT = process.env.PORT || 6000;
-
+const PRODUCTS = require('./product');
 app.use(bodyParser.json())
 
+async function startServer() {
+    const server = new ApolloServer({
+        typeDefs: `
+            type Product {
+                id: ID!
+                name: String!
+                price: Float!
+            }
+
+            type Query {
+                getProduct(id: ID!): Product
+                getAllProducts: [Product]
+            }
+        `,
+        resolvers: {
+            Query: {
+                getProduct: (parent, {id}) => PRODUCTS.find((product) => product.id == id),
+                getAllProducts: () => PRODUCTS
+            }
+        }
+    });
+    await server.start();
+    app.use('/graphql', expressMiddleware(server));
+    app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
+}
+
+startServer()
+/* 
 // GET Req
 app.get('/', (req, res) => {
     res.send("Hello aaryan");
@@ -27,6 +57,5 @@ app.post('/msg', (req, res) => {
         status: 200,
         msg: "Message retreived successfully " + msg
     });
-})
+}) */
 
-app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
---
+++ product.js
@@ -0,0 +1,14 @@
+const PRODUCTS = [
+    {
+        id: 123,
+        name: "product 1",
+        price: 0.0
+    },
+    {
+        id: 1234,
+        name: "product 2",
+        price: 0.0
+    }
+];
+
+module.exports = PRODUCTS;
